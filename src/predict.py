e969e1cf5f5165b41d2d487580f7c94c58252349
Inteleger <kdz2314461602@gmail.com>
2026-07-13 13:19:59 +0800

    finished predict.py

--- src/predict.py
+++ src/predict.py
@@ -14,38 +14,48 @@ def predict_batch(model, inputs, tokenizer, device):
         decoder_input = torch.full(size=(batch_size, 1), fill_value=tokenizer.start_token_id, device=device)
 
         generated_ids = []
+        is_finished = torch.full(size=[batch_size], fill_value=False, device=device)
         for i in range(SEQ_LEN):
             decoder_output, decoder_hidden_output = model.decoder(decoder_input, decoder_hidden_output)
             next_token_id = torch.argmax(decoder_output, dim=-1)
             generated_ids.append(next_token_id)
             decoder_input = next_token_id
+            is_finished |= (next_token_id.squeeze(1) == tokenizer.end_token_id)
+            if is_finished.all():
+                break
 
-    batch_results = torch.sigmoid(output)
-    return batch_results.tolist()
+    generated_list = torch.cat(generated_ids, dim=1).tolist()
+    for i, sentence_ids in enumerate(generated_list):
+        if tokenizer.end_token_id in sentence_ids:
+            eos_pos = sentence_ids.index(tokenizer.end_token_id)
+            generated_list[i] = sentence_ids[:eos_pos]
 
-def predict(text, model, tokenizer, device):
+    return generated_list
 
-    ids = tokenizer.encode(text, SEQ_LEN)
+def predict(text, model, cn_tokenizer, en_tokenizer, device):
+
+    ids = cn_tokenizer.encode(text)
 
     input = torch.tensor([ids], dtype=torch.long).to(device)
 
-    result = predict_batch(model, input)
+    result = predict_batch(model, input, en_tokenizer, device)
 
-    return result[0]
+    return en_tokenizer.decode(result[0])
 
 def run_predict():
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    tokenizer = JiebaTokenizer.from_vocab(MODEL_DIR/VOCAB_FILE)
+    en_tokenizer = EnglishTokenizer.from_vocab(MODEL_DIR/EN_VOCAB_FILE)
+    cn_tokenizer = ChineseTokenizer.from_vocab(MODEL_DIR/CN_VOCAB_FILE)
     print('vocabulary load success!')
 
-    model = ReviewAnalysisModel(vocab_size=tokenizer.vocab_size, padding_idx=tokenizer.pad_token_id).to(device)
+    model = TranslateModel(cn_tokenizer.vocab_size, en_tokenizer.vocab_size, cn_tokenizer.pad_token_id, en_tokenizer.pad_token_id).to(device)
     model.load_state_dict(torch.load(MODEL_DIR / BEST_MODEL))
     print('model load success!')
 
-    print('Welcome to INTELEGER sentiment analysis model! print q or quit to exit...')
+    print('Welcome to INTELEGER CH->EN translate model! print q or quit to exit...')
     input_history = ''
     while True:
-        user_input = input('> ')
+        user_input = input('> please input a Chinese sentence: ')
         if user_input.strip() in ['q', 'quit']:
             print('bye!')
             break
@@ -53,11 +63,8 @@ def run_predict():
             print('please input valid content!')
             continue
 
-        result = predict(user_input, model, tokenizer, device)
-        if result >= 0.5:
-            print(f'positive review: (confidence level: {result})')
-        else:
-            print(f'negative review: (confidence level: {1 - result})')
+        result = predict(user_input, model, cn_tokenizer, en_tokenizer, device)
+        print('In English: ', result)
 
 if __name__ == '__main__':
     run_predict()
