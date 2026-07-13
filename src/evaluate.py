e1a321ed364a45b68bdbaf6aaec0d271288574ff
Inteleger <kdz2314461602@gmail.com>
2026-07-13 13:37:10 +0800

    finished evaluate.py

--- src/evaluate.py
+++ src/evaluate.py
@@ -1,42 +1,42 @@
 import torch
 from config import *
-from model import ReviewAnalysisModel
+from model import TranslateModel
 from dataset import get_dataloader
 from predict import predict_batch
-from tokenizer import JiebaTokenizer
+from tokenizer import ChineseTokenizer, EnglishTokenizer
 from tqdm import tqdm
+from nltk.translate.bleu_score import corpus_bleu
 
-def evaluate(model, dataloader, device):
-    correct_count = 0
-    total_count = 0
+def evaluate(model, dataloader, tokenizer, device):
+    references = []
+    predictions = []
 
     model.eval()
     with torch.no_grad():
-        for inputs, targets in tqdm(dataloader, desc='Evaluating: '):
+        for inputs, targets in dataloader:
             inputs, targets = inputs.to(device), targets.to(device)
-            batch_results = predict_batch(model, inputs)
-            for target, result in zip(targets, batch_results):
-                total_count += 1
-                result = 1 if result > 0.5 else 0
-                if result == target:
-                    correct_count += 1
-    correct_rate = correct_count / total_count
-    return correct_rate
+            targets = targets.tolist()
+            batch_result = predict_batch(model, inputs, tokenizer, device)
+            predictions.extend(batch_result)
+            references.extend([[target[1:target.index(tokenizer.end_token_id)]] for target in targets])
+    bleu_score = corpus_bleu(references, predictions)
+    return bleu_score
 
 def run_evaluate():
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    tokenizer = JiebaTokenizer.from_vocab(MODEL_DIR/VOCAB_FILE)
+    cn_tokenizer = ChineseTokenizer.from_vocab(MODEL_DIR/CN_VOCAB_FILE)
+    en_tokenizer = EnglishTokenizer.from_vocab(MODEL_DIR/EN_VOCAB_FILE)
     print('vocabulary load success!')
 
-    model = ReviewAnalysisModel(vocab_size=tokenizer.vocab_size, padding_idx=tokenizer.pad_token_id).to(device)
+    model = TranslateModel(cn_tokenizer.vocab_size, en_tokenizer.vocab_size, cn_tokenizer.pad_token_id, en_tokenizer.pad_token_id).to(device)
     model.load_state_dict(torch.load(MODEL_DIR / BEST_MODEL))
     print('model load success!')
 
     test_dataloader = get_dataloader(train=False)
-    correct_rate = evaluate(model, test_dataloader, device)
+    bleu = evaluate(model, test_dataloader, en_tokenizer, device)
 
     print('evaluate result:')
-    print('correct rate: ', correct_rate)
+    print('BLEU Score: ', bleu)
 
 if __name__ == '__main__':
     run_evaluate()
